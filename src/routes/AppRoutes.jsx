2f6dbdd0a6f255c99806a0df2d0d9d2ad28088c4
fnilsson <gusnilfral@student.gu.se>
2025-04-17 15:00:29 +0200

    Add routes for Year1, Year2 and Year3

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,12 +4,18 @@ import Courses from "../pages/Courses";
 import NotFound from "../pages/NotFound";
 import Reflections from "../pages/Reflections";
 import Resources from "../pages/Resources";
+import Year1 from "../pages/Year1";
+import Year2 from "../pages/Year2";
+import Year3 from "../pages/Year3";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/courses" element={<Courses />} />
+      <Route path="/courses/year1" element={<Year1 />} />
+      <Route path="/courses/year2" element={<Year2 />} />
+      <Route path="/courses/year3" element={<Year3 />} />
       <Route path="/reflections" element={<Reflections />} />
       <Route path="/resources" element={<Resources />} />
       <Route path="*" element={<NotFound />} />
