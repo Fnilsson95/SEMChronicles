519731da282bb9e556a32011e3b60dc0b5e75d45
fnilsson <gusnilfral@student.gu.se>
2025-04-17 22:34:48 +0200

    Create yearOneCourses map to hold all courses

---
+++ src/utils/yearOneCourses.js
@@ -0,0 +1,31 @@
+import mathImg from "../assets/images/courses/dit023_math.png";
+import oopImg from "../assets/images/courses/dit043_oop.jpg";
+import reqImg from "../assets/images/courses/dit046_req.png";
+import teamImg from "../assets/images/courses/dit094_team.png";
+
+const yearOneCourses = [
+  {
+    id: 1,
+    title: "Object-Oriented Programming - DIT043",
+    path: "/courses/year1/oop",
+    image: oopImg,
+  },
+  {
+    id: 2,
+    title: "Mathematical Foundations for Software Engineering - DIT023",
+    path: "/courses/year1/math",
+    image: mathImg,
+  },
+  {
+    id: 3,
+    title: "Mini Project: Team Programming - DIT094",
+    path: "/courses/year1/team",
+    image: teamImg,
+  },
+  {
+    id: 4,
+    title: "Requirements and User Experience - DIT046",
+    path: "/courses/year1/req",
+    image: reqImg,
+  },
+];
