177e87808aead900c3cf5367d4912a8771c0076b
fnilsson <gusnilfral@student.gu.se>
2025-04-21 11:39:55 +0200

    Create TypeWriter component
    Created Typewriter functionality to have as introduction to the application

---
+++ src/components/IntroductionPage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+/* React wrapper for typewrite-style text animation */
+/* Provides an animated typing functionality */
+import Typewriter from "typewriter-effect";
+
+const Typer = () => {
+  return (
+    <div className="TypeEffect">
+      <Typewriter
+        options={{
+          string: [
+            /* Array of strings to type one by one */
+            "Software Engineering Student",
+            "University of Gothenburg",
+            "Sweden",
+          ],
+          autoStart: true /* Automatically start the animation on mount */,
+          loop: true /* Repeat/Loop the typing */,
+          delay: 10 /* Delay (ms) between each character being typed */,
+          deleteSpeed: 10 /* Speed (ms) for deleting characters */,
+        }}
+      />
+    </div>
+  );
+};
+
+export default Typer;
