bb39abd30f521cae433d6974deac5b808cda42d8
fnilsson <gusnilfral@student.gu.se>
2025-04-21 12:29:57 +0200

    Mount Typewriter to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import CircleLoader from "react-spinners/CircleLoader";
 import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 1900);
+  }, []);
+
   return (
     <div className="app-layout">
-      <Navbar />
-      <main className="main-content">
-        <AppRoutes />
-      </main>
-      <Footer />
+      {loading ? (
+        <div className="loader">
+          <CircleLoader
+            color={"#011c38"}
+            loading={true}
+            size={100}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <main className="main-content">
+            <AppRoutes />
+          </main>
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
