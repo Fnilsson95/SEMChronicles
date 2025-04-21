b303f929fa5aec2125d1f503259030b19cd3a74f
fnilsson <gusnilfral@student.gu.se>
2025-04-21 20:34:19 +0200

    Update footer with github link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 import "../styles/Footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>
-        &copy; {new Date().getFullYear()} SEMChronicles. All rights reserved
-      </p>
+      <h4>
+        &copy; {new Date().getFullYear()} SEMChronicles | All rights reserved
+      </h4>
+      <div className="footerLinks">
+        <a href="https://github.com/Fnilsson95" target="_blank">
+          <FaGithub />
+        </a>
+      </div>
     </footer>
   );
 };
