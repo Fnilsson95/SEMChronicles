e7e735be84ac9ae61146799d352ffb3b147952f9
fnilsson <gusnilfral@student.gu.se>
2025-07-15 21:13:30 +0200

    Add component and data array for Year2 page

--- src/pages/Year2.jsx
+++ src/pages/Year2.jsx
@@ -1,8 +1,21 @@
+import CourseCard from "../components/CourseCard";
+import yearTwoCourses from "../utils/yearTwoCourses";
+import "../styles/CourseCard.css";
+
 export default function Year2() {
   return (
-    <div>
-      <h1> Year 2 - Courses</h1>
-      <p> Details about Year 2: </p>
+    <div className="year-page">
+      <h1> Year 2 - Courses </h1>
+      <div className="courses-grid">
+        {yearTwoCourses.map((course) => (
+          <CourseCard
+            key={course.id}
+            title={course.title}
+            to={course.path}
+            image={course.image}
+          />
+        ))}
+      </div>
     </div>
   );
 }
